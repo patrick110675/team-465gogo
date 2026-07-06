0fc44dd619c068b15ef8e730ccdd0ff76b2ce09d
patrick110675 <f22598426@gmail.com>
2026-07-06 09:55:09 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='amr-v7-1-click-fix-cache-v2';
+const CACHE='amr-v7-2-link-navigation-cache-v1';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./css/main.css','./js/app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'])));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request)));});
